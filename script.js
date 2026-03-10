cb8c26c05b27f099925bc8cf8832c6d92cbaeda5
Eden mulugeta <mulugetaeden155@gmail.com>
2026-03-10 21:29:28 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+git add script.js
+git commit -m "Add JavaScript file"
+git push
+particlesJS("particles-js", {
+  particles: {
+    number: { value: 70 },
+    size: { value: 3 },
+    move: { speed: 2 },
+    line_linked: { enable: true }
+  }
+});
+
+const text = "Aspiring Software Developer";
+let i = 0;
+
+function typing(){
+if(i < text.length){
+document.querySelector(".typing").innerHTML += text.charAt(i);
+i++;
+setTimeout(typing,70);
+}
+}
+
+typing();sss
